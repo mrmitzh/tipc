59a7d42f0218d881acda5cb15de6e61d7c339679
mrmitzh <tianzhanhong412@gmail.com>
2019-11-21 09:58:15 -0500

    complete ast visitor

--- src/TIPAstVisitor.h
+++ src/TIPAstVisitor.h
@@ -7,26 +7,26 @@
 class TIPAstVisitor
 {
 public:
-  // TODO: check if we add all the ast node in visitor.. 
-  void  visitFunction(std::unique_ptr<TIPtree::Function> root) override;
-  void  visitNumExpr(std::unique_ptr<TIPtree::NumberExpr> root) override;
-  void  visitInputExpr(std::unique_ptr<TIPtree::InputExpr> root) override;
-  void  visitFunAppExpr(std::unique_ptr<TIPtree::FunAppExpr> root) override;
-  void  visitAllocExpr(std::unique_ptr<TIPtree::AllocExpr> root) override;
-  void  visitRefExpr(std::unique_ptr<TIPtree::RefExpr> root) override;
-  void  visitDeRefExpr(std::unique_ptr<TIPtree::DeRefExpr> root) override;
-  void  visitNullExpr(std::unique_ptr<TIPtree::NullExpr> root) override;
-  void  visitRecordExpr(std::unique_ptr<TIPtree::RecordExpr> root) override;
-  void  visitFieldExpr(std::unique_ptr<TIPtree::FieldExpr> root) override;
-  void  visitAccessExpr(std::unique_ptr<TIPtree::AccessExpr> root) override;
-  void  visitAssignableExpr(std::unique_ptr<TIPtree::AssignStmt> root) override;
-  void  visitDeclaration(std::unique_ptr<TIPtree::DeclStmt> root) override;
-  void  visitAssignmentStmt(std::unique_ptr<TIPtree::AssignStmt> root) override;
-  void  visitBlockStmt(std::unique_ptr<TIPtree::BlockStmt> root) override;
-  void  visitWhileStmt(std::unique_ptr<TIPtree::WhileStmt> root) override;
-  void  visitIfStmt(std::unique_ptr<TIPtree::IfStmt> root) override;
-  void  visitOutputStmt(std::unique_ptr<TIPtree::OutputStmt> root) override;
-  void  visitErrorStmt(std::unique_ptr<TIPtree::ErrorStmt> root) override;
-  void  visitReturnStmt(std::unique_ptr<TIPtree::ReturnStmt> root) override;
-
+  // TODO: add Function and Program attached to the syntax tree?? 
+  virtual void  visitFunction(std::unique_ptr<TIPtree::Function> root) = 0;
+  virtual void  visitNumExpr(std::unique_ptr<TIPtree::NumberExpr> root) = 0;
+  virtual void  visitVarExpr(std::unique_ptr<TIPtree::VariableExpr> root) = 0;
+  virtual void  visitBinaryExpr(std::unique_ptr<TIPtree::BinaryExpr> root) = 0;
+  virtual void  visitFunAppExpr(std::unique_ptr<TIPtree::FunAppExpr> root) = 0;
+  virtual void  visitInputExpr(std::unique_ptr<TIPtree::InputExpr> root) = 0;
+  virtual void  visitAllocExpr(std::unique_ptr<TIPtree::AllocExpr> root) = 0;
+  virtual void  visitRefExpr(std::unique_ptr<TIPtree::RefExpr> root) = 0;
+  virtual void  visitDeRefExpr(std::unique_ptr<TIPtree::DeRefExpr> root) = 0;
+  virtual void  visitNullExpr(std::unique_ptr<TIPtree::NullExpr> root) = 0;
+  virtual void  visitFieldExpr(std::unique_ptr<TIPtree::FieldExpr> root) = 0;
+  virtual void  visitRecordExpr(std::unique_ptr<TIPtree::RecordExpr> root) = 0;
+  virtual void  visitAccessExpr(std::unique_ptr<TIPtree::AccessExpr> root) = 0;
+  virtual void  visitDeclaration(std::unique_ptr<TIPtree::DeclStmt> root) = 0;
+  virtual void  visitBlockStmt(std::unique_ptr<TIPtree::BlockStmt> root) = 0;
+  virtual void  visitAssignmentStmt(std::unique_ptr<TIPtree::AssignStmt> root) = 0;
+  virtual void  visitWhileStmt(std::unique_ptr<TIPtree::WhileStmt> root) = 0;
+  virtual void  visitIfStmt(std::unique_ptr<TIPtree::IfStmt> root) = 0;
+  virtual void  visitOutputStmt(std::unique_ptr<TIPtree::OutputStmt> root) = 0;
+  virtual void  visitErrorStmt(std::unique_ptr<TIPtree::ErrorStmt> root) = 0;
+  virtual void  visitReturnStmt(std::unique_ptr<TIPtree::ReturnStmt> root) = 0;
 };
